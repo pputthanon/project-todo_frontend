15f935a71bcb9a14ba5d47d754b52f295d4cd6c8
pputthanon <p.putthanon@gmail.com>
2024-05-01 21:52:01 +0700

    edit: homepage when dont have any task

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/use-auth";
+import { IoCloseCircleSharp } from "react-icons/io5";
 import Header from "../layout/Header";
 import Modal from "../components/Modal";
 import CreateForm from "../forms/CreateForm";
@@ -25,46 +26,55 @@ export default function Homepage() {
     <div className="h-screen">
       <Header />
 
-      <div className="flex justify-center h-screen">
-        <div className="flex w-1/2 mt-10 flex-col">
-          <div className="flex justify-between bg-[#CCD5AE] w-full h-16 items-center rounded-lg px-3 text-title">
-            <div className="ml-2">LOGO</div>
-            <div className="flex justify-center items-center cursor-pointer">
-              <div
-                onClick={() => setIsOpen(true)}
-                className="mx-2 font-semibold flex"
-              >
-                Add
-              </div>
-              <Modal
-                title={"Add Task"}
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-                update={update}
-                setUpdate={setUpdate}
-              >
-                <CreateForm
+      {allTask.id ? (
+        <div className="flex justify-center h-screen">
+          <div className="flex w-1/2 mt-10 flex-col">
+            <div className="flex justify-between bg-[#CCD5AE] w-full h-16 items-center rounded-lg px-3 text-title">
+              <div className="ml-2">LOGO</div>
+              <div className="flex justify-center items-center cursor-pointer">
+                <div
+                  onClick={() => setIsOpen(true)}
+                  className="mx-2 font-semibold flex"
+                >
+                  Add
+                </div>
+                <Modal
+                  title={"Add Task"}
+                  open={isOpen}
                   onClose={() => setIsOpen(false)}
                   update={update}
                   setUpdate={setUpdate}
+                >
+                  <CreateForm
+                    onClose={() => setIsOpen(false)}
+                    update={update}
+                    setUpdate={setUpdate}
+                  />
+                </Modal>
+                {/* <div className="mx-2 ">filter</div> */}
+              </div>
+            </div>
+            <div className="">
+              {allTask?.map((el) => (
+                <TaskList
+                  key={el.id}
+                  taskObj={el}
+                  allTask={allTask}
+                  update={update}
+                  setUpdate={setUpdate}
                 />
-              </Modal>
-              {/* <div className="mx-2 ">filter</div> */}
+              ))}
             </div>
           </div>
-          <div className="">
-            {allTask?.map((el) => (
-              <TaskList
-                key={el.id}
-                taskObj={el}
-                allTask={allTask}
-                update={update}
-                setUpdate={setUpdate}
-              />
-            ))}
-          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen font-semibold text-2xl flex-col gap-10">
+          <div className="text-9xl">
+            <IoCloseCircleSharp />
+          </div>
+          <div> Looks like you don't have any tasks yet!</div>
+        </div>
+      )}
     </div>
   );
 }
